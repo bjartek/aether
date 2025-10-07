6da106bf11e656ba756ba46a08a4ad27c6092507
Bjarte Stien Karlsen <bjarte@bjartek.org>
2025-10-07 11:44:31 +0200

    added changes to debug why it does not work

--- main.go
+++ main.go
@@ -5,12 +5,10 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/bjartek/aether/pkg/aether"
 	"github.com/bjartek/aether/pkg/flow"
 	"github.com/bjartek/aether/pkg/logs"
 	"github.com/bjartek/aether/pkg/ui"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/psiemens/graceland"
 )
 
 //go:embed cadence/FCL.cdc
@@ -30,37 +28,51 @@ func main() {
 	)
 
 	// Create a zerolog logger that outputs to the TUI logs view
-	logger := logs.NewLogger(p)
+	logger, err := logs.NewLoggerWithFile(p, "aether.log")
+	if err != nil {
+		panic(err)
+	}
 
-	// Example: Start a goroutine that generates some log messages
+	fmt.Println("test1")
+	// Initialize emulator and dev wallet
+	emu, dw, err := flow.InitEmulator(&logger)
+	if err != nil {
+		logger.Error().Err(err).Msg("failed to initialize Flow emulator & dev wallet")
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("test2")
+
+	// Start emulator in background
 	go func() {
-		dw, emu, err := flow.InitEmulator(&logger)
-		if err != nil {
-			panic(err)
+		logger.Info().Msg("Starting Flow emulator...")
+		emu.Start()
+		logger.Info().Msg("Emulator stopped")
+	}()
+	fmt.Println("test3")
+
+	// Start dev wallet in background
+	go func() {
+		logger.Info().Msg("Starting dev wallet...")
+		if err := dw.Start(); err != nil {
+			logger.Error().Err(err).Msg("Dev wallet stopped with error")
 		}
-
-		gl := graceland.NewGroup()
-
-		dw.AddToGroup(gl)
-		gl.Add(emu)
-		gl.Add(&aether.Aether{
-			Logger: &logger,
-			FclCdc: fclCdc,
-		})
-
-		fmt.Println("lets goo")
-		err = gl.Start()
-		if err != nil {
-			logger.Error().Err(err).Msg("❗  Server error")
-		}
-		fmt.Println("foobar")
-		gl.Stop()
 	}()
 
+	fmt.Println("test4")
 	// Run the program
 	if _, err := p.Run(); err != nil {
 		logger.Error().Err(err).Msg("Error running program")
 		fmt.Printf("Error: %v\n", err)
+		emu.Stop()
+		dw.Stop()
 		os.Exit(1)
 	}
+
+	fmt.Println("test5")
+
+	// Clean shutdown
+	logger.Info().Msg("Shutting down services...")
+	emu.Stop()
+	dw.Stop()
 }
